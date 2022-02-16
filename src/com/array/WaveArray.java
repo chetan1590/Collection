a7c9c88b661012baf0ca18eaaf34b14b66da1621
Chetan Jain <jain.chetan1590@gmail.com>
2022-02-16 11:27:25 +0530

    Wave Array

--- src/com/array/WaveArray.java
+++ src/com/array/WaveArray.java
@@ -1,7 +1,18 @@
 package com.array;
 
+import java.util.Arrays;
+
 public class WaveArray {
 	public static void main(String[] args) {
+		int[] input = { 10, 5, 4, 3, 2, 1 };
+
+		for (int i = 0; i < input.length - 1; i+=2) {
+			int temp = input[i];
+			input[i] = input[i + 1];
+			input[i + 1] = temp;
+		}
+
+		System.out.println(Arrays.toString(input));
 
 	}
 }
